20bdee910b63c522a8f020befd9444b1aae3b515
MATHEW <milanbins1233@gmail.com>
2021-04-26 14:25:22 +0530

    commit and connect firebase

--- app/src/main/java/com/example/testapp/login1.kt
+++ app/src/main/java/com/example/testapp/login1.kt
@@ -29,6 +29,7 @@ class login1 : AppCompatActivity() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         supportActionBar?.hide()
         actionBar?.hide();
+
         val num1 = findViewById<EditText>(R.id.email)
         val num2 = findViewById<EditText>(R.id.password)
         val imgbtn = findViewById<Button>(R.id.login_button)
